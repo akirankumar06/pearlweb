fe997d7248e9c3d014f14c88bf778e42df30aae3
akirankumar06 <kiranhemu24@gmail.com>
2024-08-05 14:10:11 +0530

    feat: fixed the navigation bar

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function closeAnnouncement() {
     document.getElementById('announcement').style.display = 'none';
   }
+  const navLinks = document.querySelector('.nav-links')
+        function onToggleMenu(e){
+            e.name = e.name === 'menu' ? 'close' : 'menu'
+            navLinks.classList.toggle('top-[23%]')
+        }
